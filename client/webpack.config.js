45981469b91f67e969efd572923eec6931cc3a0e
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-08-16 17:50:59 -0700

    added indexeddb connection, service workers, updated webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,31 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE'
       }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Takes notes with JS syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
